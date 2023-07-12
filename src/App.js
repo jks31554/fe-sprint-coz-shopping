2a3f9b30a0205b02946750779b7c09bd59c578c8
jks31554 <jks31554@naver.com>
2023-07-12 18:33:15 +0900

    main page 작성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-// import { Component } from 'react';yarn add react-router-dom
+import React from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  /* flex: 1; */
+`;
 
 function App() {
   return (
-    <div className='App'>
-      <header>
-        <h1>dd</h1>
-      </header>
-      <main></main>
-      <footer></footer>
-    </div>
+    <AppContainer>
+      <Header />
+      <Main></Main>
+      <Footer />
+    </AppContainer>
   );
 }
 
